9866a1d1a0f5f6033ffdc8b43c3bcd24ea480206
Brandon Lilly <brandonjlilly@gmail.com>
2016-06-23 14:37:00 -0700

    remove unnecessary dependencies

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import Banner from './Banner'
-import EventDisplay from './EventDisplay'
 import Footer from './Footer'
-import Hero from './Hero'
-import Nav from './Nav'
 
 const App = ({ template, data, children }) =>
   <div className="App">
